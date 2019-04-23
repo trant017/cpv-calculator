8d69e01f8d66153f52407584d1e7e1b462b3d2d4
trant017 <trant017@gmail.com>
2019-04-23 19:20:48 -0400

    Updates

---
+++ precache-manifest.a1796b5a28f47d3307145d62e5d851e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d319826484480040a5c",
+    "url": "/trant017/cpv-calculator/static/js/runtime~main.e7532f32.js"
+  },
+  {
+    "revision": "ca8528621f1f7fb817ab",
+    "url": "/trant017/cpv-calculator/static/js/main.10f9fbd7.chunk.js"
+  },
+  {
+    "revision": "1d4ddf963f1564a7ee06",
+    "url": "/trant017/cpv-calculator/static/js/2.4986472a.chunk.js"
+  },
+  {
+    "revision": "ca8528621f1f7fb817ab",
+    "url": "/trant017/cpv-calculator/static/css/main.9be129d7.chunk.css"
+  },
+  {
+    "revision": "3f01908951cf97d2466b35877a1436ed",
+    "url": "/trant017/cpv-calculator/index.html"
+  }
+];
